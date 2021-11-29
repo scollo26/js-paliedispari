efb1aa30e6155b0bf2fc41b9ce6df94fc76b1cdc
scollo26 <giovanniscollo26@gmail.com>
2021-11-29 14:49:14 +0100

    pari e dispari

---
+++ js/script.js
@@ -0,0 +1,106 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+let parola = prompt('inserisci parola');
+
+// let carattere = parola.split('');
+// carattere = carattere.reverse();
+// let palindrome = carattere.join('');
+
+// console.log(palindrome);
+
+let somma = palindrome(parola);
+
+
+
+if (parola == palindrome) {
+
+    console.log('parola palindrome');
+    
+}else{
+    console.log('non sono palindrome');
+}
+
+
+
+function palindrome(parola) {
+
+    let palindrome = parola.split('').reverse().join('');
+    return palindrome;
+    
+}
+
+
+// // Pari e Dispari
+// // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// // Generiamo un numero random (sempre da 1 a 5) per il computer
+// // Sommiamo i due numeri
+// // Stabiliamo se la somma dei due numeri è pari o dispari
+// // Dichiariamo chi ha vinto
+
+// let scelta = prompt('pari o dispari')
+// // let numberUsers = parseInt(prompt('inserisci numero'));
+// // let numbPc = parseInt(prompt('inserisci numero'));
+
+// let numberUsers = parseInt(prompt('inserisci numero'));
+
+// let numbPc = Math.floor(Math.random() * 5) + 1;
+
+
+// console.log(numberUsers);
+// console.log(numbPc);
+
+// numberTotal = numbPc + numberUsers;
+
+// if (numberTotal % 2 == 0) {
+    
+//     console.log(numberTotal )
+//     // invocazione-funzione-pari
+//     let valore = pari(pari);
+
+//     // if(scelta == 'pari'){
+//     //     console.log('vinto pari');
+//     // }else{
+//     //     console.log('hai perso pari');
+//     // }
+
+// }else if(numberTotal % 2 != 0){
+//     console.log(numberTotal )
+//     // invocazione-funzione-dispari
+//     let valore = dispari(dispari);
+//     // if(scelta == 'dispari'){
+//     //     console.log('vinto dispari');
+//     // }else{
+//     //     console.log('hai perso dispari');
+//     // }
+// }
+
+
+// // funzione-pari
+// function pari(pari) {
+//     let risultato = '';
+//     if(scelta == 'pari'){
+//         risultato = 'pari';
+//         console.log('vinto pari');
+//     }else{
+//         console.log('hai perso pari');
+//         risultato = 'dispari';
+//     }
+// return risultato
+    
+// }
+
+// // funzione-dispari
+// function dispari(dispari) {
+//     let risultato = '';
+//     if(scelta == 'dispari'){
+//         risultato = 'dispari';
+//         console.log('vinto dispari');
+//     }else{
+//         console.log('hai perso dispari');
+//         risultato = 'pari';
+//     }
+// return risultato
+    
+// }
