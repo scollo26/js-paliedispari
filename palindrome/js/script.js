eab8d6a86dde99f70cb77de0ba2ada4cd110d038
scollo26 <giovanniscollo26@gmail.com>
2021-11-29 15:47:20 +0100

    pari e dispari -funzione

--- palindrome/js/script.js
+++ palindrome/js/script.js
@@ -1,8 +1,28 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-
+// primo-metodo
 let parola = prompt('inserisci parola');
+
+let parolaReverse ="";
+
+for(let i = parola.length - 1; i >= 0; i--){
+    parolaReverse = parolaReverse + parola[i];
+}
+
+console.log('parola inserita ' + parola);
+console.log('parola al contrario ' + parolaReverse);
+
+
+
+if (parola == parolaReverse) {
+    console.log('la parola ' + parola + ' è palindroma');
+}else {
+    console.log('la parola ' + parola + ' non è palindroma');
+}
+
+
+// secondo metodo
 
 // let carattere = parola.split('');
 // carattere = carattere.reverse();
@@ -10,32 +30,31 @@ let parola = prompt('inserisci parola');
 
 // console.log(palindrome);
 
-let parolaInversa = palindrome(parola);
+// let parolaInversa = palindrome(parola);
 
-console.log(parolaInversa);
+// console.log(parolaInversa);
 
-if (parola == parolaInversa) {
+// if (parola == parolaInversa) {
 
-    console.log('parola palindrome');
-    document.writeln(parola)
-    document.writeln(parolaInversa + ' sono palindrome')
+//     console.log('parola palindrome');
+//     document.writeln(parola)
+//     document.writeln(parolaInversa + ' sono palindrome')
     
     
-}else{
-    console.log('non sono palindrome');
-    document.writeln(parola)
-    document.writeln(parolaInversa + ' non sono palindrome')
-}
+// }else{
+//     console.log('non sono palindrome');
+//     document.writeln(parola)
+//     document.writeln(parolaInversa + ' non sono palindrome')
+// }
 
 
 
-function palindrome(parola) {
+// function palindrome(parola) {
 
-    let palindrome = parola.split('').reverse().join('');
-    return palindrome;
+//     let palindrome = parola.split('').reverse().join('');
+//     return palindrome;
     
-}
-
+// }
 
 
 
--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 // // Pari e Dispari
 // // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // // Generiamo un numero random (sempre da 1 a 5) per il computer
@@ -13,19 +5,24 @@
 // // Stabiliamo se la somma dei due numeri è pari o dispari
 // // Dichiariamo chi ha vinto
 
-let scelta = prompt('pari o dispari')
+let scelta = prompt('Pari o Dispari')
 // let numberUsers = parseInt(prompt('inserisci numero'));
 // let numbPc = parseInt(prompt('inserisci numero'));
 
 let numberUsers = parseInt(prompt('inserisci numero'));
 
-let numbPc = Math.floor(Math.random() * 5) + 1;
+// let numbPc = Math.floor(Math.random() * 5) + 1;
 
 
 console.log(numberUsers);
+
+// invocazione-random
+let numbPc = random(1, 5);
+
 console.log(numbPc);
 
-numberTotal = numbPc + numberUsers;
+// somma-due  numeri
+let numberTotal = numbPc + numberUsers;
 
 if (numberTotal % 2 == 0) {
     
@@ -50,15 +47,21 @@ if (numberTotal % 2 == 0) {
     // }
 }
 
+// numeri-random
+function random(min, max) {
+    let random = Math.floor(Math.random() * 5) + 1;
+    
+    return random
+}
 
 // funzione-pari
 function pari(pari) {
     let risultato = '';
     if(scelta == 'pari'){
         risultato = 'pari';
-        console.log('vinto pari');
+        console.log('vinto è pari');
     }else{
-        console.log('hai perso pari');
+        console.log('hai perso è pari');
         risultato = 'dispari';
     }
 return risultato
@@ -70,11 +73,12 @@ function dispari(dispari) {
     let risultato = '';
     if(scelta == 'dispari'){
         risultato = 'dispari';
-        console.log('vinto dispari');
+        console.log('vinto è dispari');
     }else{
-        console.log('hai perso dispari');
+        console.log('hai perso è dispari');
         risultato = 'pari';
     }
 return risultato
     
 }
+
