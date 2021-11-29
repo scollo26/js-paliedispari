42eaa08113af55c313a2c0f859f804fe23385eab
scollo26 <giovanniscollo26@gmail.com>
2021-11-29 17:11:39 +0100

    commenti palindrome

--- palindrome/js/script.js
+++ palindrome/js/script.js
@@ -2,59 +2,60 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // primo-metodo
-let parola = prompt('inserisci parola');
+// let parola = prompt('inserisci parola');
 
-let parolaReverse ="";
+// let parolaReverse ="";
 
-for(let i = parola.length - 1; i >= 0; i--){
-    parolaReverse = parolaReverse + parola[i];
-}
+// for(let i = parola.length - 1; i >= 0; i--){
+//     parolaReverse = parolaReverse + parola[i];
+// }
 
-console.log('parola inserita ' + parola);
-console.log('parola al contrario ' + parolaReverse);
+// console.log('parola inserita ' + parola);
+// console.log('parola al contrario ' + parolaReverse);
 
 
 
-if (parola == parolaReverse) {
-    console.log('la parola ' + parola + ' è palindroma');
-}else {
-    console.log('la parola ' + parola + ' non è palindroma');
-}
+// if (parola == parolaReverse) {
+//     console.log('la parola ' + parola + ' è palindroma');
+// }else {
+//     console.log('la parola ' + parola + ' non è palindroma');
+// }
 
 
 // secondo metodo
+let parola = prompt('inserisci parola');
 
-// let carattere = parola.split('');
-// carattere = carattere.reverse();
-// let palindrome = carattere.join('');
+let carattere = parola.split('');
+carattere = carattere.reverse();
+let palindrome = carattere.join('');
 
-// console.log(palindrome);
+console.log(palindrome);
 
-// let parolaInversa = palindrome(parola);
+let parolaInversa = palindrome(parola);
 
-// console.log(parolaInversa);
+console.log(parolaInversa);
 
-// if (parola == parolaInversa) {
+if (parola == parolaInversa) {
 
-//     console.log('parola palindrome');
-//     document.writeln(parola)
-//     document.writeln(parolaInversa + ' sono palindrome')
+    console.log('parola palindrome');
+    document.writeln(parola)
+    document.writeln(parolaInversa + ' sono palindrome')
     
     
-// }else{
-//     console.log('non sono palindrome');
-//     document.writeln(parola)
-//     document.writeln(parolaInversa + ' non sono palindrome')
-// }
+}else{
+    console.log('non sono palindrome');
+    document.writeln(parola)
+    document.writeln(parolaInversa + ' non sono palindrome')
+}
 
 
 
-// function palindrome(parola) {
+function palindrome(parola) {
 
-//     let palindrome = parola.split('').reverse().join('');
-//     return palindrome;
+    let palindrome = parola.split('').reverse().join('');
+    return palindrome;
     
-// }
+}
 
 
 
